4cfa7f86c9c7206fcccbdfaede7e0141165064f8
radiaated <iamsumankht@gmail.com>
2026-03-20 16:51:33 +0545

    refactor: add CSRF origins

--- web/web/settings.py
+++ web/web/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -143,3 +143,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    host.strip()
+    for host in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if host.strip()
+]
